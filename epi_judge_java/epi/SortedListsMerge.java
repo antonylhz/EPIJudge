89e44a1377d7a2ca7a61c7b3a2028f88361dd114
Hongzhang Liu <hongzhal@amazon.com>
2018-03-18 15:27:36 -0400

    Sorted Lists Merge

--- epi_judge_java/epi/SortedListsMerge.java
+++ epi_judge_java/epi/SortedListsMerge.java
@@ -6,10 +6,22 @@ import epi.test_framework.GenericTestHandler;
 public class SortedListsMerge {
   @EpiTest(testfile = "sorted_lists_merge.tsv")
   //@include
-  public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> L1,
-                                                      ListNode<Integer> L2) {
-    // Implement this placeholder.
-    return null;
+  public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> n1,
+                                                      ListNode<Integer> n2) {
+      ListNode<Integer> dum = new ListNode<Integer>(0, null),
+              cur = dum;
+      while (n1 != null && n2 != null) {
+          if (n1.data <= n2.data) {
+              cur.next= n1;
+              n1 = n1.next;
+          } else {
+              cur.next = n2;
+              n2 = n2.next;
+          }
+          cur = cur.next;
+      }
+      cur.next = n1 == null ? n2 : n1;
+      return dum.next;
   }
 
   public static void main(String[] args) {
